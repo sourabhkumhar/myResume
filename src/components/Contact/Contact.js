e0e511911131fd335233cc3817e5a434e5a13ac7
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2022-09-16 12:53:14 +0530

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Social from '../Social'
 
 export default function Contact(props) {
   return (
     <>
-      
+      <div className="m-5">
+        This page is under-development.
+        Please contact me on <Social site="gmail" gmail="dev.sourabhkumhar@gmail.com" />
+      </div>
     </>
   )
 }
