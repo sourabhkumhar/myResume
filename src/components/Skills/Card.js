8df13eb1c0c01d305c488345f829968f99e4c33d
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2022-10-13 23:41:11 +0530

    update

--- src/components/Skills/Card.js
+++ src/components/Skills/Card.js
@@ -5,13 +5,11 @@ export default function Card(props) {
     return (
         <div id={props.img} className="col-4 card p-0" style={{ width: "251px", margin: '10px' }}>
             <div className='cardBox'>
-                <img src={process.env.PUBLIC_URL + `/images/${props.img}.svg`} className="" width="250px" height="250px" alt="..." />
+                <img src={process.env.PUBLIC_URL + `/images/${props.img}.svg`} className="" width="250px" height="250px" alt="..." draggable="false" />
             </div>
 
             <div className="card-body" align="center">
                 <h5 className="card-title" style={{color: "#14213D"}}>{props.title}</h5>
-                {/* <p className="card-text text-justify">{props.desc}</p> */}
-                {/* <button disabled={!props.projectLink} href="/" className="btn btn-primary">See Projects</button> */}
             </div>
         </div>
     )
--- src/components/Social.js
+++ src/components/Social.js
@@ -5,19 +5,19 @@ const Social = (props) => {
     return (
         <>
             {site === "Github" && <a className='pe-3' rel="noreferrer" target="_blank" href={`https://${site}.com/sourabhkumhar`}>
-                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} />
+                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} draggable="false" />
             </a>}
             
             {site === "Instagram" && <a className='pe-3' rel="noreferrer" target="_blank" href={`https://${site}.com/sourabh__kumhar`}>
-                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} />
+                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} draggable="false" />
             </a>}
 
             {site === "Linkedin" && <a className='pe-3' rel="noreferrer" target="_blank" href={`https://${site}.com/in/sourabhkumhar`}>
-                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} />
+                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} draggable="false" />
             </a>}
 
             {site === "gmail" && <a className='pe-3' href={"mailto:" + gmail}>
-                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} />
+                <img src={process.env.PUBLIC_URL + `/images/${site}.png`} width="35px" alt={site} draggable="false" />
             </a>}
         </>
     )
