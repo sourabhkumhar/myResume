096097fef795c6c74cb570acaeb4e5d59f6d0193
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2023-02-18 00:29:06 +0530

    Update skills and resume

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -10,12 +10,9 @@ export default function Skills() {
       <div className='row justify-content-center'>
         <h1 className="subHeading text-center ">Basics</h1>
 
-
-        <Card img="html" title="HTML" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
-
-        <Card img="css" title="CSS" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
-
-        <Card img="js" title="JavaScript" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
+        <Card img="html" title="HTML" />
+        <Card img="css" title="CSS" />
+        <Card img="js" title="JavaScript" />
       </div>
 
       <hr className='w-50 text-black mx-auto' align="center" />
@@ -23,9 +20,10 @@ export default function Skills() {
       <div className='row justify-content-center mt-5'>
         <h1 className="subHeading text-center">Frameworks</h1>
 
-        <Card img="react" title="React" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
-
-        <Card img="bootstrap" title="Bootstrap" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
+        <Card img="react" title="React" />
+        <Card img="next" title="Next" />
+        <Card img="tailwind" title="Tailwind" />
+        <Card img="bootstrap" title="Bootstrap" />
       </div>
 
       <hr className='w-50 text-black mx-auto' align="center" />
@@ -33,9 +31,8 @@ export default function Skills() {
       <div className='row justify-content-center mt-5'>
         <h1 className="subHeading text-center">Backend</h1>
 
-        <Card img="nodejs" title="Node.js" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
-
-        <Card img="php" title="PHP" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
+        <Card img="nodejs" title="Node.js" />
+        <Card img="php" title="PHP" />
       </div>
 
       <hr className='w-50 text-black mx-auto' align="center" />
@@ -43,9 +40,8 @@ export default function Skills() {
       <div className='row justify-content-center mt-5'>
         <h1 className="subHeading text-center">Database</h1>
 
-        <Card img="mongodb" title="MongoDB" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
-
-        <Card img="mysql" title="MySQL" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ad, incidunt ullam sed optio deserunt ipsum mollitia nemo obcaecati magni!" />
+        <Card img="mongodb" title="MongoDB" />
+        <Card img="mysql" title="MySQL" />
       </div>
     </>
   )
